1141f217714ec0f63f201a7fc9613fa294892d76
Carlos CC <carloscc9297@gmail.com>
2025-11-26 13:58:54 +0100

    :construction: doing whole user CRUD

--- chat_ya_backend/Endpoints/UserEndpoints.cs
+++ chat_ya_backend/Endpoints/UserEndpoints.cs
@@ -4,23 +4,35 @@ using chat_ya_backend.Models.Dtos.RequestDtos;
 using chat_ya_backend.Models.Dtos.ResponseDtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Serilog;
 using System.Security.Claims;
 
 namespace chat_ya_backend.Endpoints
 {
     public static class UserEndpoints
     {
+        /// <summary>
+            /// La palabra clave this permite "extender" la clase WebApplication. 
+            /// Esto significa que, en Program.cs, puedes simplemente escribir app.MapUserEndpoints(); como si fuera un método nativo de la aplicación
+            /// Este método registrará(o mapeará) todas las rutas(los endpoints) que se definan dentro de él.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
         public static WebApplication MapUserEndpoints(this WebApplication app)
         {
-            // Creamos un grupo de rutas base /api/users, todas requieren autorización
+            // Creamos un grupo de rutas base /api/users
             var group = app.MapGroup("/api/users")
-                           .RequireAuthorization()
+                           .RequireAuthorization()//todos los endpoints de este grupo solo se pueden acceder si el cliente envia un token JWT válido
                            .WithOpenApi();
 
             // Reutilizamos el Logger Factory
             var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
-            var userLogger = loggerFactory.CreateLogger("UserEndpoints");
+            var userLogger = loggerFactory.CreateLogger("UserEndpoints");//sistema para obtener mensajes en la consola
 
+
+            /// ClaimsPrincipal user. El objeto user contiene la identidad de la persona que hizo la solicitud
+            /// UserManager<IdentityUser> userManager: herramienta principal de ASP.NET Identity para interactuar con la tabla de usuarios. 
+            /// Lo usamos para buscar, crear o actualizar usuarios.
             // --- 1. GET /api/users/me (Obtener Perfil Propio) ---
             group.MapGet("/me", async (ClaimsPrincipal user, UserManager<IdentityUser> userManager) =>
             {
@@ -32,7 +44,7 @@ namespace chat_ya_backend.Endpoints
                 }
 
                 // 2. Buscar el usuario en Identity
-                var identityUser = await userManager.FindByIdAsync(userId);
+                var identityUser = await userManager.FindByIdAsync(userId); 
                 if (identityUser == null)
                 {
                     // Esto no debería pasar si el token es válido
