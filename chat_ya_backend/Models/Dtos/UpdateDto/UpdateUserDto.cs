d19fa59f60c6dd0497234f5703a585c6936b3d9d
Carlos CC <carloscc9297@gmail.com>
2025-11-27 11:04:24 +0100

    :sparkles: Full CRUD for User

---
+++ chat_ya_backend/Models/Dtos/UpdateDto/UpdateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace chat_ya_backend.Models.Dtos.UpdateDto
+{
+    public class UpdateUserDto
+    {
+        public string? NewUserName { get; set; }
+        public string? NewEmail { get; set; }
+    }
+}
